c294193d5cb9e36e060fa08aaeb7678c8be2d688
unknown <kdouglasrono@gmail.com>
2022-10-17 17:03:37 +0300

    serving a json file

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 
 //app.use(express.static(__dirname + "/public"));
+//serving a static assets(photos,styles,etc);
 app.use('/public', express.static(__dirname + "/public"));
 //also works
 //app.use('/public/style.css', express.static(__dirname + "/public/style.css"));
@@ -14,6 +15,9 @@ app.get("/", function(req,res){
     res.sendFile(__dirname + "/views/index.html");
 })
 
+app.get("/json",function(req,res){
+    res.json({"message":"Hello json"});
+})
 
 
 
