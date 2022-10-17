65fb8b11a3baca82819d2474b85ab4cec7ca2e63
unknown <kdouglasrono@gmail.com>
2022-10-17 07:02:42 +0300

    serving static files

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 let express = require('express');
 let app = express();
+
+//app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + "/public"));
 console.log("Hello World");
 /*app.get("/", function(req,res){
   res.send("Hello Express");
@@ -8,6 +11,7 @@ console.log("Hello World");
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/views/index.html");
 })
+
 
 
 
