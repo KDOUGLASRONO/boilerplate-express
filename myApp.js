1dcc98254890fbed327a8c2356194329303360ac
unknown <kdouglasrono@gmail.com>
2022-10-17 07:08:50 +0300

    serving static files

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 //app.use(express.static(__dirname + "/public"));
 app.use('/public', express.static(__dirname + "/public"));
+//also works
+//app.use('/public/style.css', express.static(__dirname + "/public/style.css"));
 console.log("Hello World");
 /*app.get("/", function(req,res){
   res.send("Hello Express");
